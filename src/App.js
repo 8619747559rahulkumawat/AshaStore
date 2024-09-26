b5fbc3d79ba6a9c985219091621f7402e757b419
Rahul Kumawat <8619747559rahulkumawat>
2024-09-26 14:30:51 +0530

    Creste app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Home from './Com/Webpage/Home/Home';
+import Header from './Com/Webpage/Header/Herader'
+import { Routes, Route } from 'react-router-dom';
+import Men from './Com/Webpage/Men/Men'
+import Women from './Com/Webpage/Women/Women'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+   
+     <Routes>
+      <Route path='/' element={ <Home/>}/>
+      <Route path='/Men' element={<Men/>}/>
+      <Route path='/Women' element={<Women/>}/>
+
+
+     </Routes>
     </div>
   );
 }
---
+++ src/Com/Webpage/Header/Herader.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import './Header.css'
+import AppsIcon from '@mui/icons-material/Apps';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+
+import { Link } from 'react-router-dom';
+
+const Herader = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+       
+      </List>
+      <Divider />
+      <div className='Sidebharmanu'>
+      <ul>
+      <Link to='/'><li> Home</li></Link>
+      
+       
+      <Link to='/Men'>  <li>  Men</li></Link>
+        <Link to='/Women'><li> Women</li></Link>
+      </ul>
+      </div>
+
+    </Box>
+  );
+  return (
+    <div>
+      <header className="bg-primary py-4 px-6 md:px-12 flex justify-between items-center bg-red-500 text-white">
+        <img className="logo" src="img/logo.jpeg" alt="E-commerce Logo" />
+        <nav className="hidden md:flex space-x-4">
+         <div className="data">
+         <ul>
+          <Link to='/'><li> Home</li></Link>
+      
+       
+        <Link to='/Men'>  <li>  Men</li></Link>
+          <Link to='/Women'><li> Women</li></Link>
+         </ul>
+         </div>
+
+
+        </nav>
+        <div className="flex items-center">
+          <div className='manu'>
+            <input type="text" placeholder="Search..." className="bg-input text-primary-foreground px-3 py-1 rounded-md mr-2 focus:outline-none" />
+            <Button className="Serach" variant="contained">Search</Button>
+            <AccountCircleIcon />
+          </div>
+          <div className='Sidebhar'>
+            <Button onClick={toggleDrawer(true)}><AppsIcon /></Button>
+            <Drawer open={open} onClose={toggleDrawer(false)}>
+              {DrawerList}
+            </Drawer>
+          </div>
+        </div>
+
+      </header>
+    </div>
+  )
+}
+
+export default Herader
