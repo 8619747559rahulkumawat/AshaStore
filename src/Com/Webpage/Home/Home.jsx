9e392c5971735902966a71a43ccd1636a9307dec
Rahul Kumawat <8619747559rahulkumawat>
2024-09-27 15:37:42 +0530

    datarahulkumawat

--- src/Com/Webpage/Home/Home.jsx
+++ src/Com/Webpage/Home/Home.jsx
@@ -1,26 +1,27 @@
 import React from 'react'
 import './Home.css'
-<<<<<<< HEAD
+import FilterComponent from '../FilterComponent/FilterComponent'
 import Slider from  '../Slider/Slider'
 import Secure from '../Secure/Secure';
 import Reviews from '../Reviews/Reviews'
 import InstagramFeed from '../InstagramFeed/InstagramFeed'
 import Footer from '../Footer/Footer'
-=======
 
->>>>>>> 2170973a5ed20feea836362f4a7063371df62fcb
+
+
 const Home = () => {
 
   
   return (
     <div>
    <Slider/>
+   <FilterComponent />
    <Secure/>
    <Reviews/>
    <InstagramFeed/>
 <Footer/>
    
-      <FilterComponent />
+ 
     </div>
   )
 }
